ab7749f25d4efff735f97a80ea73b633b89bafc3
Doramisi1 <105469411+Doramisi1@users.noreply.github.com>
2023-02-19 08:32:38 +0000

    Add files via upload

---
+++ help.js
@@ -0,0 +1,18 @@
+
+exports.infoOwner = () =>{
+return`──「 *DATA PROFIL OWNER* 」──
+
+ ⬣ *Nama :* ega-ganteng
+ ⬣ *Umur :* 100
+ ⬣ *Hoby. :* Depan Komputer
+ ⬣ *Asal   :* sumut-medan
+ ⬣ *Status:* Creator
+ ⬣ *Zodiak:* Gk tau
+
+                  「 *SOSIAL MEDIA*」
+ ⬣ *Whatsapp:* 083849469400
+ ⬣ *Youtube:* youtube.com/@melliafriska
+ ⬣ *Instagram:* instagram.com/bertoananta
+ ⬣ *website:* mellhost.panellkuu.my.id
+ `
+}
